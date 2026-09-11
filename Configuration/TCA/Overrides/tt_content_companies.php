759b4a3861d68e581458adfaecca6f835d166768
Christian Wolfram <c.wolfram@chriwo.de>
2026-09-11 12:28:13 +0200

    [TASK] Migrate plugins into content elements (#2)

---
+++ Configuration/TCA/Overrides/tt_content_companies.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+(static function () {
+    $pluginSignature = ExtensionUtility::registerPlugin(
+        'Contacts',
+        'Companies',
+        'LLL:EXT:contacts/Resources/Private/Language/locallang_db.xlf:tx_contacts.plugin.companies',
+        'module-contacts',
+        'contacts',
+        'LLL:EXT:contacts/Resources/Private/Language/locallang_db.xlf:tx_contacts.plugin.companies.description',
+    );
+
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        '--div--;LLL:EXT:contacts/Resources/Private/Language/locallang_tca.xlf:tt_content.div.contacts_configuration,pi_flexform',
+        $pluginSignature,
+        'after:subheader'
+    );
+
+    ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        'FILE:EXT:contacts/Configuration/FlexForms/CompaniesPlugin.xml',
+        $pluginSignature
+    );
+})();
